3ddec27cd89d8f219526f4942ec8b41d3e0fb952
trantrinh1102 <trantrinh.1102@gmail.com>
2016-08-09 14:46:44 +0700

    update test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost = current_user.microposts.build if logged_in?
     @feed_items = current_user.feed.paginate(page: params[:page],:per_page => 5)
+    end
   end
 
   def help
